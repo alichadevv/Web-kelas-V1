5e495096977d847374bb5dd6c6d76fdba9f8bad9
メイチャ <alichaicha248@gmail.com>
2025-05-14 15:04:04 +0700

    Add files via upload

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -5,7 +5,7 @@ const Kamis = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Teknik Jaringan</div>
+				<div className="w-[50%] text-base  font-medium">Sejarah Kebudayaan IsIam</div>
 				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
 			</div>
 			<div
@@ -19,7 +19,7 @@ const Kamis = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">Penjaskes</div>
+				<div className="w-[50%] text-base  font-medium">Al-Qur'an Dan Hadis</div>
 				<div className="flex justify-center items-center text-sm">10.20-11.40</div>
 			</div>
 			<div
@@ -34,7 +34,14 @@ const Kamis = () => {
 				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="1000">
-				<div className="w-[50%] text-base font-medium">TKJ</div>
+				<div className="w-[50%] text-base font-medium">Fikih</div>
+				<div className="flex justify-center items-center text-sm">12.20-13.40</div>
+			</div>
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">Bahasa Inggris</div>
 				<div className="flex justify-center items-center text-sm">12.20-13.40</div>
 			</div>
 		</>
